1998b485586b8d3d21851fe0aa63dc44d4dda7ff
Anupam-USP <ianupam1509@gmail.com>
2021-05-14 16:10:20 +0530

    Rec 2

--- Tutorial/Recursion/invCount.cpp
+++ Tutorial/Recursion/invCount.cpp
@@ -2,40 +2,44 @@
 using namespace std;
 #define ll long long
 
-void merge(int *a, int s, int e)
+int merge(int *a, int s, int e)
 {
     int mid = (s + e) / 2;
     int i = s;
     int j = mid + 1;
     int k = s;
+    int tmp[100000], cnt = 0;
 
-    int tmp[100];
-    while (i <= mid && j <= e)
+    while (i <= mid and j <= e)
     {
-        if (a[i] < a[j])
+        if (a[i] <= a[j])
             tmp[k++] = a[i++];
         else
+        {
             tmp[k++] = a[j++];
+            cnt += mid - i + 1;
+        }
     }
 
     while (i <= mid)
         tmp[k++] = a[i++];
-
     while (j <= e)
         tmp[k++] = a[j++];
-
     for (int i = s; i <= e; i++)
         a[i] = tmp[i];
+    return cnt;
 }
 
-void mergeSort(int *a, int s, int e)
+int inv_Count(int *a, int s, int e)
 {
     if (s >= e)
-        return;
+        return 0;
+
     int mid = (s + e) / 2;
-    mergeSort(a, s, mid);
-    mergeSort(a, mid + 1, e);
-    merge(a, s, e);
+    int x = inv_Count(a, s, mid);
+    int y = inv_Count(a, mid + 1, e);
+    int z = merge(a, s, e);
+    return x + y + z;
 }
 
 int main()
@@ -50,9 +54,6 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    mergeSort(a, 0, n - 1);
-
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+    cout << inv_Count(a, 0, n - 1) << endl;
     return 0;
 }
